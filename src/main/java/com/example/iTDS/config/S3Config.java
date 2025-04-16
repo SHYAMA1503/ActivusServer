bdda46a14625bacfbc88c029fbc5d92bac83a1f4
SHYAMA1503 <shyamyobel@outlook.com>
2025-04-17 02:21:34 +0530

    Initial commit

--- src/main/java/com/example/iTDS/config/S3Config.java
+++ src/main/java/com/example/iTDS/config/S3Config.java
@@ -4,18 +4,23 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
+    @Value("${aws.accessKey}")
+    private String accessKey;
 
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
     @Bean
     public AmazonS3 amazonS3() {
-        // Replace these with your actual AWS credentials
-        String accessKey = "AKIAXZEFIEVLRE2GPXH3";
-        String secretKey = "egfgZraJ9CyPUe8UqEGkkWsB+Cwm8UUut66HA7bT";
-        String region = "eu-north-1"; // e.g., "us-east-1"
+        
 
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder
