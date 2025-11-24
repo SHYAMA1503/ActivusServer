5cf347082e38f41a7bd607b782b0996e7fbf6616
SHYAMA1503 <shyamyobel@outlook.com>
2025-11-24 00:40:58 +0000

    Comment out S3Config class and its dependencies
    Comment out the entire S3Config class and its contents.

--- src/main/java/com/example/iTDS/config/S3Config.java
+++ src/main/java/com/example/iTDS/config/S3Config.java
@@ -1,33 +1,33 @@
-package com.example.iTDS.config;
+// package com.example.iTDS.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import com.amazonaws.auth.AWSStaticCredentialsProvider;
+// import com.amazonaws.auth.BasicAWSCredentials;
+// import com.amazonaws.services.s3.AmazonS3;
+// import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class S3Config {
-    @Value("${aws.accessKey}")
-    private String accessKey;
+// @Configuration
+// public class S3Config {
+//     @Value("${aws.accessKey}")
+//     private String accessKey;
 
-    @Value("${aws.secretKey}")
-    private String secretKey;
+//     @Value("${aws.secretKey}")
+//     private String secretKey;
 
-    @Value("${aws.region}")
-    private String region;
-    @Bean
-    public AmazonS3 amazonS3() {
+//     @Value("${aws.region}")
+//     private String region;
+//     @Bean
+//     public AmazonS3 amazonS3() {
 
 
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return AmazonS3ClientBuilder
-                .standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                .build();
-    }
-}
+//         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+//         return AmazonS3ClientBuilder
+//                 .standard()
+//                 .withRegion(region)
+//                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+//                 .build();
+//     }
+// }
 
